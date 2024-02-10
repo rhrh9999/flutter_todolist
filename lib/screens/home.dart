6ec6716ab3f6b91ead44ad37090985369e1b7e50
rhrh9999 <112358232+rhrh9999@users.noreply.github.com>
2024-02-10 21:18:11 +0900

    search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,10 +10,43 @@ class Home extends StatelessWidget {
         backgroundColor: toBGColor,
         appBar: _buildAppBar(),
       body: Container(
-        child: Text('This is home screen.'),
+        padding: EdgeInsets.symmetric(horizontal:15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
     ),
     );
   }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon( //돋보기아이콘
+            Icons.search,
+            color: toBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            maxWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: toGrey),
+        ),
+      ),
+    );
+  }
+
 
   AppBar _buildAppBar(){
     return AppBar(
