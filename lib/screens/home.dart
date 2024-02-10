8819715f64671d4f1ab10993550421daf7f33669
rhrh9999 <112358232+rhrh9999@users.noreply.github.com>
2024-02-10 14:44:26 +0900

    project setting made directory and dart.file

---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todolist_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: toBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Icon(
+            Icons.menu,
+            color: toBlack,
+            size:30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child:Image.asset('assets/images/avatar.jpg'),
+            )
+          )
+        ],)
+      ),
+      body: Container(
+        child: Text('This is home screen.'),
+      )
+    );
+  }
+}
