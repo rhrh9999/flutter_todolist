54414beb32a3c050135bd5556e6b45ae37b40381
rhrh9999 <112358232+rhrh9999@users.noreply.github.com>
2024-02-11 16:18:43 +0900

    UI) bottom of 'Add a new todo item' and '+'

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,15 +13,17 @@ class Home extends StatelessWidget {
     return Scaffold(
         backgroundColor: toBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-            horizontal:20,
-            vertical: 15
-        ),
-        child: Column(
-          children: [
-            searchBox(),
-              Expanded(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+                horizontal:20,
+                vertical: 15
+            ),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
                   child: ListView(
                     children: [
                       Container(
@@ -35,14 +37,65 @@ class Home extends StatelessWidget {
 
                       for ( ToDo todoo in todosList)
                         ToDoItem(todo: todoo),
+
                     ],
                   ),
-              )
-          ],
-        ),
-    ),
-
-
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right:20,
+                    left: 20,
+                  ),
+                  padding:EdgeInsets.symmetric(
+                     horizontal: 20,
+                     vertical: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    ),],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add a new todo item',
+                      border: InputBorder.none
+                    ),
+                  ),
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      bottom:20,
+                      right: 20,
+                  ),
+                  child: ElevatedButton(
+                    child: Text('+', style: TextStyle(fontSize: 40,),),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: toBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10,
+                    ),
+                    ),
+                ),
+              ],
+            )
+          )
+        ],
+      ),
     );
   }
 
